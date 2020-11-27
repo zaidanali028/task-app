a05f318b2c76824b30be8b9ab0a73a1500a3a5a0
Bernard Arhia <55264725+bernardarhia@users.noreply.github.com>
2020-11-27 17:20:49 +0000

    updated the auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,22 @@
 const jwt=require('jsonwebtoken')
 const User=require('../models/User')
 
-module.exports={
-  
-  userAuth: async function (req,res,next){
-    try{
-      const token=req.header('Authorization').replace('Bearer ','')
-      const verifyData=jwt.verify(token,'myfirstjsw')
-      const user = await User.findOne({_id:verifyData._id,'userTokens.token':token})
-      if(!user){
-        throw new Error()
-      }
+const auth = async (req, res, next)=>{
+    
+    try {
+        
+        const token = req.header('Authorization').replace('Bearer ','');
+        const verifyData = jwt.verify(token, 'put your jwt secret here');
+        const user = await User.findOne({_id:verifyData._id, 'userTokens.token':token});
 
-      req.token = token
-      req.user=user
-      next()
+        if(!user) {throw new Error()};
+        req.token = token
+        req.user = user;
+        next();
+
+    } catch (error) {
+        res.status(401).send({error:'Please Login'})
     }
-    catch (e) {
-      res.status(401).send({error: "Please login"});
-  }
+}
 
-
-
-  }
-           
-  
-} 
+module.exports = auth;
